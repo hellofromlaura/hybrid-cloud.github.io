160634f01417fd383407abd4b585200149ff0667
Laura <laurawolfart@gmail.com>
2022-11-03 17:56:46 -0400

    fixing animation issue

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function isInViewport(el) {
 
 const box1 = document.querySelector('#animate-stats1');
 const box2 = document.querySelector('#animate-stats2');
-const box3 = document.querySelector('#animate-stats3');
+// const box3 = document.querySelector('#animate-stats3');
 
 var bar1 = new ProgressBar.Circle(circle1, {
   strokeWidth: 8,
@@ -42,18 +42,18 @@ var bar2 = new ProgressBar.Circle(circle2, {
 });
 
 
-var bar3 = new ProgressBar.Circle(circle3, {
-  strokeWidth: 8,
-  easing: 'easeInOut',
-  duration: 1400,
-  color: '#6ABD4A',
-  trailColor: '#E2E2E2',
-  trailWidth: 8,
-  svgStyle: null,
-  step: (state, bar) => {
-    bar.setText(Math.round(bar.value() * 100) + '%');
-  }
-});
+// var bar3 = new ProgressBar.Circle(circle3, {
+//   strokeWidth: 8,
+//   easing: 'easeInOut',
+//   duration: 1400,
+//   color: '#6ABD4A',
+//   trailColor: '#E2E2E2',
+//   trailWidth: 8,
+//   svgStyle: null,
+//   step: (state, bar) => {
+//     bar.setText(Math.round(bar.value() * 100) + '%');
+//   }
+// });
 
 document.addEventListener('scroll', function () {
   if (isInViewport(box1)) {
@@ -62,7 +62,7 @@ document.addEventListener('scroll', function () {
   if (isInViewport(box2)) {
     bar2.animate(0.79);  // Number from 0.0 to 1.0
   }
-  if (isInViewport(box3)) {
-    bar3.animate(0.53);  // Number from 0.0 to 1.0
-  }
+  // if (isInViewport(box3)) {
+  //   bar3.animate(0.53);  // Number from 0.0 to 1.0
+  // }
 });
